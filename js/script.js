a8fa5fab00e8145045f1332b5d1f9e651bb1386d
GiadaTonioni <giada.tonioni@gmail.com>
2023-05-24 14:31:33 +0200

    lista in console

---
+++ js/script.js
@@ -0,0 +1,29 @@
+let listaSpesa = [
+    'Scamorza',
+    'Funghi',
+    'Piadine',
+    'Zucchine',
+    'Carote',
+    'Hummus',
+    'Pasta',
+    'Porro',
+    'Sughi',
+    'Pomodorini'
+];
+console.log(listaSpesa)
+
+
+let lista = document.getElementById('lista')
+console.log(lista)
+
+let sequenza = 0 
+while(sequenza < listaSpesa.length) {
+    console.log(listaSpesa[sequenza])
+    sequenza++;
+
+}
+    
+
+
+
+
